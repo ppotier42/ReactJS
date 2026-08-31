18de07b44f0b10d8c40537882ed73119968a6781
pedro <pierre.podnieks@gmail.com>
2026-08-31 18:20:43 +0200

    Auth is now Working properly

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,11 +1,13 @@
 import {useContext, useState} from "react";
-import {useForm} from "react-hook-form";
+import {set, useForm} from "react-hook-form";
 import { AuthContext} from "../context/AuthContext.jsx";
+import { useNavigate } from "react-router-dom";
 
 export default function Auth() {
     const [mode, setMode] = useState("signup")
-    const { signUp, user } = useContext(AuthContext)
-    
+    const [error, setError] = useState(null);
+    const { signUp, user, logout, login } = useContext(AuthContext)
+    const navigate = useNavigate();
     const {
         register,
         handleSubmit, 
@@ -13,18 +15,31 @@ export default function Auth() {
     } = useForm();
     
     function onSubmit(data) {
-        signUp(data.email, data.password);
+        let result;
+        setError(null)
+        if (mode === "signup") {
+            result = signUp(data.email, data.password);
+        } else {
+            result = login(data.email, data.password);
+        }
+        if (result.success) {
+            navigate("/");
+        } else {
+            setError(result.error);
+        }
     }
     
     return (
         <div className={"page"}>
             <div className={"container"}>
                 <div className={"auth-container"}>
-                    {user && <p>User Loggedin: {user.email} </p>}
+                    {user && <p>User LoggedIn: {user.email} </p>}
+                    <button onClick={() => logout()}>Logout</button>
                     <h1 className={"page-title"}>
                         {mode === "signup" ? "Sign Up" : "Login"}
                     </h1>
                     <form className={"auth-form"} onSubmit={handleSubmit(onSubmit)}>
+                        {error && <div className={"error-message"}>{error}</div>}
                         <div className={"form-group"}>
                             <label className={"form-label"} htmlFor={"email"}>Email</label>
                             <input 
